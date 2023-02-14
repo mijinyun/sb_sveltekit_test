635a30a9b6812fefaefedd4a3274f1a8877f1cef
mijinyun <yunmj1224@gmail.com>
2023-02-14 16:30:44 +0900

    error check

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
-import { env } from '$env/dynamic/public'
+// import { env } from '$env/dynamic/public'
+import { PUBLIC_SUPABASE_URL,PUBLIC_SUPABASE_ANON_KEY } from '$lib/Env'
 
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
+// export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
