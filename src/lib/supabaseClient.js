6fd7ce01c9be8dd3d7787166329b5cdfa75a6d94
mijinyun <yunmj1224@gmail.com>
2023-02-14 17:38:32 +0900

    url error test3

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,5 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
-// import { env } from '$env/dynamic/public'
+import { env } from '$env/dynamic/public'
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
 
