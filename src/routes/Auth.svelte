772a21b2cb855774b18b1da68368a5d8a991c550
mijinyun <yunmj1224@gmail.com>
2023-02-14 20:20:43 +0900

    setting: remove layout.server.js

--- src/routes/Auth.svelte
+++ src/routes/Auth.svelte
@@ -9,8 +9,7 @@
             loading = true;
             const { error } = await supabase.auth.signInWithOtp({email});
 
-            if (error) throw console.log('auth-page-handlelogin:',error);
-            alert('Check your email for the login link!');
+            if (error) throw alert('Check your email for the login link!');
         } catch (error) {
             if (error instanceof Error) {
                 alert(error.message);
